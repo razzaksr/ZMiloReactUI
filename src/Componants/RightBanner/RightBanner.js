4f4eead747f63c6110fbbb06fb0f6c4192622f68
razzaksr <razzaksr@gmail.com>
2023-01-19 13:00:16 +0530

    right bar done

--- src/Componants/RightBanner/RightBanner.js
+++ src/Componants/RightBanner/RightBanner.js
@@ -3,7 +3,39 @@ import "./RightBanner.css"
 export default function RightBanner() {
   return (
     <div className="banright">
-        <p>Right banner</p>
+        <div className="wrapright">
+          <div className="birthdayContainer">
+            <img src="/props/gift.png" className="giftImg"/>
+            <span className="giftText">
+              <b>Sabarinathan</b> and <b>3 other friends</b>have a birthday today
+            </span>
+          </div>
+          <img className="ad" src="/props/ad.png"/>
+          <h4 className="rtTitle">Online friends</h4>
+          <ul className="rtFriendList">
+            <li className="rtFriend">
+              <div className="rtProfileImgContainer">
+                <img className="rtProfileImg" src="/props/person/2.jpeg"/>
+                <span className="rtOnline"></span>
+              </div>
+              <span className="rtUser">Kevin nash</span>
+            </li>
+            <li className="rtFriend">
+              <div className="rtProfileImgContainer">
+                <img className="rtProfileImg" src="/props/person/2.jpeg"/>
+                <span className="rtOnline"></span>
+              </div>
+              <span className="rtUser">Kevin nash</span>
+            </li>
+            <li className="rtFriend">
+              <div className="rtProfileImgContainer">
+                <img className="rtProfileImg" src="/props/person/2.jpeg"/>
+                <span className="rtOnline"></span>
+              </div>
+              <span className="rtUser">Kevin nash</span>
+            </li>
+          </ul>
+        </div>
     </div>
   )
 }

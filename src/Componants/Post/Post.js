5590fce9b07ea4874d9e110f5f95a4625679a15f
razzaksr <razzaksr@gmail.com>
2023-01-20 12:28:29 +0530

    Like event handled

--- src/Componants/Post/Post.js
+++ src/Componants/Post/Post.js
@@ -2,8 +2,18 @@ import "./Post.css"
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import CommentIcon from '@mui/icons-material/Comment';
 import { Users } from "../../dummyData";
+import { useState } from "react";
 
 export default function Post(attr) {
+
+    const[like,setLike]=useState(attr.post.like)
+    const[isLiked,setIsLiked]=useState(false)
+
+    const eveLike=()=>{
+        setLike(isLiked?like-1:like+1)
+        setIsLiked(!isLiked)
+    }
+
   return (
     <div className="pst">
         <div className="wrappost">
@@ -23,9 +33,9 @@ export default function Post(attr) {
             </div>
             <div className="postBottom">
                 <div className="postBottomLeft">
-                    <img className="likeIcon" src="/props/like.png"/>
-                    <img className="likeIcon" src="/props/heart.png"/>
-                    <span className="postLikeCounter">{attr.post.like} people liked it</span>
+                    <img className="likeIcon" onClick={eveLike} src="/props/like.png"/>
+                    <img className="likeIcon" onClick={eveLike} src="/props/heart.png"/>
+                    <span className="postLikeCounter">{like} people liked it</span>
                 </div>
                 <div className="postBottomRight">
                     <CommentIcon className="comIcom"/>
